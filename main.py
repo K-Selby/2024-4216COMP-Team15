1e0dc31d04b50b8cb217036bceddd0d2b51652d9
Kieran Selby <K.Selby@2023.ljmu.ac.uk>
2024-03-21 13:49:04 +0000

    Input Validation

--- main.py
+++ main.py
@@ -10,20 +10,27 @@ print("3. Race & States - KS")
 print("4. Year & States - RP")
 print("5. Threat Level & Mental Illness - JA")
 print("6. Race & Date - TW")
+print("Q. Quit Program")
 
-userInput = input("Enter the menu number for the visuallisation you want to view")
-
-
-match userInput:
-    case '1':
-        print("You have chosen Mental Illness & Fleeing")
-    case '2':
-        print("You have chosen Fleeing & Race")
-    case '3':
-        print("You have chosen Race & States")
-    case '4':
-        print("You have chosen Year & States")
-    case '5':
-        print("You have chosen Threat Level & Mental Illness")
-    case '6':
-        print("You have chosen Race & Date")
+while True:
+    userInput = input("Enter the menu number for the visuallisation you want to view")
+    
+    match userInput:
+        case '1':
+            print("You have chosen Mental Illness & Fleeing")
+        case '2':
+            print("You have chosen Fleeing & Race")
+        case '3':
+            print("You have chosen Race & States")
+        case '4':
+            print("You have chosen Year & States")
+        case '5':
+            print("You have chosen Threat Level & Mental Illness")
+        case '6':
+            print("You have chosen Race & Date")
+        case 'Q':
+            print("Quit Program")
+            break
+        case _:
+            print("Invalid Input. Try again")
+            
