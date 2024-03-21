8f5f0b8302b0a304dd0016ec2cd0179279a2596a
Ethan Beaumont <CSMEBEAU@ljmu.ac.uk>
2024-03-21 11:50:44 +0000

    commiting

--- Ethan-Mentally-ill-fleeing/Ethan-mentallyill-flee.py
+++ Ethan-Mentally-ill-fleeing/Ethan-mentallyill-flee.py
@@ -1,8 +1,11 @@
+#importing packages
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#function for pie chart
 def pie():
+    #style for pie chart
     plt.style.use('_mpl-gallery-nogrid')
 
     # reading data from file
@@ -14,7 +17,7 @@ def pie():
     mentill = data[values_column].sum()
     notmentill = len(data) - mentill
 
-    #taking values out of fleeing
+    #finding flee column
     fleecol = 'flee'
 
     #count of not fleeing
@@ -38,12 +41,12 @@ def pie():
     #creating colours for pie chart
     colours_flee_notflee = ['#ff9999','#66b3ff']
 
-    # plot
+    # plotting both pie charts
     fig, axs = plt.subplots(1,2,figsize=(10,5))
-    #mentally ill
+    #mentally ill pie chart
     axs[0].pie([mentflee, mentnotflee], labels = [f'fleeing\n({mentflee})',f'not fleeing\n({mentnotflee})'], autopct='%1.1f%%', startangle=70, wedgeprops={"linewidth": 1, "edgecolor": "white"}, colors= colours_flee_notflee)
 
-
+    #altering pie chart size ans setting title
     axs[0].axis('off')
     axs[0].set_aspect('equal')
     axs[0].set_title('mental illness')
@@ -53,15 +56,18 @@ def pie():
     #not mentally ill
     axs[1].pie([notmentflee, notmentnotflee], labels = [f'fleeing\n({notmentflee})', f'not fleeing\n({notmentnotflee})'], autopct='%1.1f%%', startangle=70, wedgeprops={"linewidth": 1, "edgecolor": "white"}, colors = colours_flee_notflee)
 
+    #aaltering pie chart size and adding title
     axs[1].axis('off')
     axs[1].set_aspect('equal')
     axs[1].set_title('not mental illness')
 
     axs[1].tick_params(axis='both', which='major', labelsize=10)
 
+    #adding main title
     plt.suptitle('Fleeing vs not fleeing')
     plt.subplots_adjust(top=0.85)
 
+    #displaying pie chart
     plt.show()
 
 pie()
